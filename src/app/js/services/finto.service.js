fbe05bbbcee4eb54166184298e3bf9688d8311df
Suominen Satu <satu.suominen@digia.com>
2018-09-19 10:27:55 +0300

    Added language parameter to keyword search

--- src/app/js/services/finto.service.js
+++ src/app/js/services/finto.service.js
@@ -4,10 +4,8 @@ angular.module('FintoService', [])
 .service('FintoService', ['$http', '$log', 'API_BASE_URL',
     function($http, $log, API_BASE_URL) {
 
-    //TODO: add language as parameter
     this.etsiAvainsanat = function(input, lang) {
-      return $http.get(API_BASE_URL +'haku/avainsanat?q=' + input);
-      // return $http.get(API_BASE_URL +'haku/avainsanat?q=' + input + '?lang=' + lang);
+      return $http.get(API_BASE_URL +'haku/avainsanat?q=' + input + '?lang=' + lang);
     };
 
 }]);
