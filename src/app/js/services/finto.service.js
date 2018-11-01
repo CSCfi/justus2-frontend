2c4e64c1e61d4594da5a3a8fed814bece73cc76f
Suominen Satu <satu.suominen@digia.com>
2018-11-01 16:44:46 +0200

    Fix bug in keyword search url

--- src/app/js/services/finto.service.js
+++ src/app/js/services/finto.service.js
@@ -5,7 +5,7 @@ angular.module('FintoService', [])
     function($http, $log, API_BASE_URL) {
 
     this.etsiAvainsanat = function(input, lang) {
-      return $http.get(API_BASE_URL +'haku/avainsanat?q=' + input + '?lang=' + lang);
+      return $http.get(API_BASE_URL +'haku/avainsanat?q=' + input + '&lang=' + lang);
     };
 
 }]);
