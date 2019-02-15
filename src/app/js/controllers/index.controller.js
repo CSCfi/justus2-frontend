4bb9e5e2c7662f57a138bc5b31c1903aa4a72e9a
Satu <satu.suominen@digia.com>
2019-02-15 15:04:57 +0200

    CI pipeline testing v2

--- src/app/js/controllers/index.controller.js
+++ src/app/js/controllers/index.controller.js
@@ -45,7 +45,7 @@ angular.module('IndexController', [])
         $scope.initialRole = $scope.user.role;
         AuthService.storeUserInfo($scope.user);
 
-        console.log("Testing CI...");
+        console.log("Testing CI vol2...");
       })
 
       .catch(function(error) {
