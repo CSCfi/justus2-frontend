0faaaa496e72a51e03612f2dbed3f88f749b0540
Satu <satu.suominen@digia.com>
2019-06-10 08:35:22 +0300

    Move bytesToSize function location

--- src/app/js/controllers/julkaisunsyotto.controller.js
+++ src/app/js/controllers/julkaisunsyotto.controller.js
@@ -120,6 +120,13 @@ angular.module('JulkaisunsyottoController', [])
                 return API_BASE_URL + "download/" + $stateParams.id ;
             };
 
+            $scope.bytesToSize = function(a,b) {
+                {if(0==a)return"0 Bytes";
+                    let c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));
+                    return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+
+            };
+
 
             $scope.version = [
             "0", "1", "2"
