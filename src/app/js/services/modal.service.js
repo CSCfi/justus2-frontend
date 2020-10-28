a0c238376ed75339859b20889911bfdc877733dd
Satu <satu.suominen@digia.com>
2020-10-28 16:35:19 +0200

    Add modal components (related to previous commit).

---
+++ src/app/js/services/modal.service.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('ModalService', [])
+.service('ModalService', ['$rootScope', '$state', '$uibModal', 'JustusService', function ($rootScope, $state, $uibModal, JustusService) {
+
+    this.openWarningModal = function(state) {
+        let modalInstance = $uibModal.open({
+            animation: false,
+            templateUrl: 'js/shared/initialize-justus-modal.html',
+            controller: 'InitializeJustusModalInstanceCtrl',
+            size: 'md',
+            backdrop: 'static',
+            resolve: {
+                lang: function () {
+                    return $rootScope.lang
+                }
+            }
+        });
+        modalInstance.result.then(function (event) {
+    
+            $state.go(state);
+            JustusService.clearPublicationForm();
+            JustusService.clearFileData();
+
+            if (state === "justus?vaihe=1") {
+                $state.reload('justus');
+            }
+
+        }, function () {
+            console.log('Edit modal dismissed at: ' + new Date());
+        });
+    }
+
+
+}])
